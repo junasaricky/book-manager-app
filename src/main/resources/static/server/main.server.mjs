ddef8e9cd17903ba482b3ea17ce50f1be909e04f
junasaricky <junasa.ricky@gmail.com>
2025-06-19 17:04:12 +0800

    Updated Angular

--- src/main/resources/static/server/main.server.mjs
+++ src/main/resources/static/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DVDL4R6M.mjs";import"./chunk-TWIF7JMK.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
+import{a}from"./chunk-YQ3YLL4G.mjs";import"./chunk-TWIF7JMK.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
